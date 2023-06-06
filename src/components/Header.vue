c29bf2a27acd4e4599c58e70d8cec3a49a685c1d
Leonardo Abraham Alonzo Martinez <leonardo.a@cliker.mx>
2023-06-06 15:45:07 -0600

    Iterando sobre el state y que son las direcctivas en vue

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<script setup>
+    import { ref, reactive } from 'vue';
+
+    const state = reactive({
+        guitarras:[]
+    })
+
+    console.log(state.guitarras)
+
+
+</script>
+
+<template>
+    
+</template>
+
+<style scoped>
+    
+</style>    
