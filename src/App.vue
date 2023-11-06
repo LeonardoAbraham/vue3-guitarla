9bc7a1736f367d19d2cbdd570cec761dde1361b4
Leonardo Abraham Alonzo Martinez <leonardo.a@cliker.mx>
2023-11-06 08:06:36 -0600

    Vaciar carrito

--- src/App.vue
+++ src/App.vue
@@ -45,6 +45,10 @@
     const eliminarProducto = (id) => {
         carrito.value = carrito.value.filter(producto => producto.id !== id)
     }
+
+    const vaciarCarrito = () => {
+        carrito.value = []
+    }
  
 </script>
 
@@ -56,6 +60,7 @@
         @decrementar-cantidad="decrementarCantidad"
         @agregar-carrito="agregarCarrito"
         @eliminar-producto="eliminarProducto"
+        @vaciar-carrito="vaciarCarrito"
     />
 
     <main class="container-xl mt-5">
